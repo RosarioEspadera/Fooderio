7bbe07a4f84a6e3d4e0033ec4ef9abdcf0b87ecf
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-17 12:25:57 +0800

    Update vite.config.js

--- fooderio-js/vite.config.js
+++ fooderio-js/vite.config.js
@@ -5,7 +5,7 @@ import react       from '@vitejs/plugin-react';
 export default defineConfig({
   define: {
     'import.meta.env': {
-      VITE_SUPABASE_URL: '"https://roqikwfaenwqipdydhwv.supabase.co:"',
+      VITE_SUPABASE_URL: '"https://roqikwfaenwqipdydhwv.supabase.co"',
       VITE_SUPABASE_ANON_KEY: '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno"',
     },
   },
