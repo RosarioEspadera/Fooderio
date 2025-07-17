65937745278a7f3d3f767ab590e604a19051f831
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-17 06:29:50 +0000

    deploy: move dist contents to root

--- fooderio-js/vite.config.js
+++ fooderio-js/vite.config.js
@@ -8,10 +8,12 @@ export default defineConfig({
     build: {
     outDir: 'dist' // 👈 this should be here
   },
+
+  base: '/Fooderio/', // Matches your repo name!
   plugins: [react()],
   define: {
     'import.meta.env': {
-      VITE_SUPABASE_URL: '"https://roqikwfaenwqipdydhwv.supabase.co:"',
+      VITE_SUPABASE_URL: '"https://roqikwfaenwqipdydhwv.supabase.co"',
       VITE_SUPABASE_ANON_KEY: '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno"',
     },
   },
